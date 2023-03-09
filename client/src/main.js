8fdab630ad12e2a157bd5e023ea77e6e6983b34e
DivineDemon <supame123@gmail.com>
2023-03-09 17:26:01 +0500

    :hammer: :fire:

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faChevronDown, faBars } from "@fortawesome/free-solid-svg-icons";
 import {
+  faBars,
+  faXmark,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
   faFacebook,
   faInstagram,
-  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
 import App from "./App.vue";
@@ -15,6 +19,7 @@ import "./assets/css/tailwind.css";
 
 // Icons
 library.add(faBars);
+library.add(faXmark);
 library.add(faTwitter);
 library.add(faFacebook);
 library.add(faInstagram);
