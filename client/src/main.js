212145dace2aa519543af742a986e8b4a378a4d2
izhan1511 <izhan47@gmail.com>
2023-03-13 12:00:04 +0500

    Rethinking the Frontend! 🔥🔨

--- client/src/main.js
+++ client/src/main.js
@@ -1,34 +1,6 @@
 import { createApp } from "vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faBell,
-  faBars,
-  faXmark,
-  faChevronDown,
-} from "@fortawesome/free-solid-svg-icons";
-import {
-  faTwitter,
-  faFacebook,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-
 import App from "./App.vue";
-import store from "./store";
 import router from "./router";
-import "./assets/css/tailwind.css";
+import store from "./store";
 
-// Icons
-library.add(faBell);
-library.add(faBars);
-library.add(faXmark);
-library.add(faTwitter);
-library.add(faFacebook);
-library.add(faInstagram);
-library.add(faChevronDown);
-
-createApp(App)
-  .use(store)
-  .use(router)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .mount("#app");
+createApp(App).use(store).use(router).mount("#app");
---
+++ server/controllers/recipeController.js
@@ -0,0 +1,9 @@
+const { connection } = require("../utils/db");
+
+const addRecipe = (req, res) => {
+  try {
+    const { name, description, ingredients, }
+  } catch (error) {
+    
+  }
+}
