ddc92e048b190fd6cb8e11978a0e0302f298c72b
DivineDemon <supame123@gmail.com>
2023-05-22 18:25:28 +0500

    Added Custom Error Handler! ✨🚀

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorHandler");
 
 // App Initialization
 const app = express();
 
-// JSON BigInt Parsing Support
+// JSON BigInt Parsing Support for Prisma
 BigInt.prototype.toJSON = function () { return this.toString() };
 
 // Middleware
 app.use(cors());
+app.use(errorHandler);
 app.use(express.json({ limit: "200mb", extended: true }));
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
