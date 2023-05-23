ede5c9a7bfa200ea1e908f417b5e7a92eba52ee6
DivineDemon <supame123@gmail.com>
2023-05-23 14:25:33 +0500

    Persistent Store + Libraries! ✨🚀💄

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from "vue";
+import { MotionPlugin } from "@vueuse/motion";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faGithub,
+  faLinkedin,
+  faFacebook,
+  faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
 import "@/assets/css/tailwind.css";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(faGithub, faFacebook, faLinkedin, faStackOverflow);
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(MotionPlugin)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,21 @@
 import { createStore } from "vuex";
+import { vuexLocal } from "@/plugins/persist";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    darkMode: false,
+  },
+  getters: {
+    getDarkMode(state) {
+      return state.darkMode;
+    },
+  },
+  mutations: {
+    setDarkMode(state, payload) {
+      state.darkMode = payload;
+    },
+  },
   actions: {},
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
